266b687c0bdcc204162f5966b5b71d04e3e5ea79
Manuraj Siddhu <manurajsiddhu@gmail.com>
2026-02-11 08:22:42 +0530

    Update setup-command.sh

--- 01-webserver-ec2/setup-command.sh
+++ 01-webserver-ec2/setup-command.sh
@@ -1,1 +1,25 @@
 
+---
+
+# 2️⃣ setup-commands.sh
+
+Create file with:
+
+```bash
+#!/bin/bash
+
+# Update system
+sudo yum update -y
+
+# Install nginx
+sudo yum install nginx -y
+
+# Start nginx
+sudo systemctl start nginx
+
+# Enable nginx on boot
+sudo systemctl enable nginx
+
+# Check status
+sudo systemctl status nginx
+```
